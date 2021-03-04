4663a241a4ba1eff90fd1b75674f44205c334a24
patricchubaca <patric_job@outlook.com>
2021-03-04 11:40:36 -0300

    att

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="pokemons">
-    
   <div class="card">
   <div class="card-image">
     <figure >
-      <img :src="pokemons.front" alt="Placeholder image">
+      <img :src="currentImg" alt="Placeholder image">
     </figure>
   </div>
   <div class="card-content">
     <div class="media">
       <div class="media-left">
-       
       </div>
       <div class="media-content">
-         <h1 class="title is-4"> {{name | upper}} {{num}}</h1>
+         <h1 class="title is-4">Nome Pokemon: {{num}} - {{name | upper}}</h1>
+         <h2 class="title is-4">Type Pokemon: {{pokemons.type | upper}}</h2>
       </div>
     </div>
-
     <div class="content">
-  
-   
+         <button @click="mudarSprite" class="button is-medium is-fullwidth">Mudar Imagem</button>
     </div>
   </div>
-</div>
   </div>
+  </div>  
      
 </template>
 
@@ -35,10 +32,13 @@ export default {
         this.pokemons.type = res.data.types[0].type.name;
         this.pokemons.front = res.data.sprites.front_default;
         this.pokemons.back = res.data.sprites.back_default;
+        this.currentImg = this.pokemons.front;
       })
     },
     data(){
       return{
+        isFront: true,
+        currentImg: '',
         pokemons: {
           type: '',
           front:'',
@@ -56,7 +56,19 @@ export default {
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
     }
+  },
+  methods:{
+    mudarSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg = this.pokemons.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemons.front;
+      }
+    }
   }
+
 }
 </script>
 <style scoped>
